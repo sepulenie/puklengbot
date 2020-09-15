deacbddbc45165c7a541eeb10626c27c04dbfc80
summe <maximreutov@gmail.com>
2020-09-15 23:06:05 +0300

    a

--- wakeup.py
+++ wakeup.py
@@ -7,12 +7,26 @@ cursor.execute("""CREATE TABLE  IF NOT EXISTS   dickdump(word_1 text, word_2 tex
 
 message = 'Одна рыба две рыбы три'
 
+
+def next_word(first_word):
+    search = "SELECT * WHERE word_1=?"
+    cursor.execute(search, [first_word])
+    search_result = (cursor.fetchone())
+
 def markov(message):
         message_split = message.split()
         first_word = random.choice(message_split)
         print(first_word)
+        chain = [first_word]
+        n_words = random.randint(1, 60)
+        next_word(first_word)
 
-markov(message)
+#markov(message)
+
+
+
+
+
 
 '''
 def markov(update, context):
