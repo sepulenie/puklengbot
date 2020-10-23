a6b0539130332ae300625db9d5da49c8aee93e61
Ubuntu <ubuntu@ip-172-31-39-236.us-east-2.compute.internal>
2020-10-23 11:35:42 +0300

    aaa

--- wakeup.py
+++ wakeup.py
@@ -5,7 +5,7 @@ cursor = conn.cursor()
 cursor.execute("""CREATE TABLE  IF NOT EXISTS   dickdump(chat_id integer, word_0 text, word_1 text)""")
 
 
-search = "DELETE FROM dickdump WHERE word_1 LIKE '%io/%'"
+search = "DELETE FROM dickdump WHERE word_1 LIKE '%s=19%'"
 cursor.execute(search)
 conn.commit()
 print(cursor.fetchall())
