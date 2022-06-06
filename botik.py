eef401f16667ff6df1b811aa21480df14cb0849b
sepulenie <maximreutov@gmail.com>
2022-06-06 15:50:16 +0300

    added dictionary

--- botik.py
+++ botik.py
@@ -1,88 +1,36 @@
 '''
-ver. 0.1.4 test
+ver. 0.1.6 dic
 '''
 import random, sqlite3, logging, urllib3, re
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from teleconfig import token
-from generator import markov_generator
+from generator import add_words_in_message_to_dictionary, generate_shitpost
 logging.basicConfig(filename='bot.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 https = urllib3.PoolManager()
 conn = sqlite3.connect("dickdump.db", check_same_thread=False)
 cursor = conn.cursor()
 cursor.execute("""CREATE TABLE IF NOT EXISTS dickdump(chat_id integer, word_0 text, word_1 text)""")
+conn.commit()
 markov_chance = 100
 
 
 def start(update):
     chat_id = update.message.chat.id
     update.message.reply_text('ID чата - ', chat_id)
-
-
-def make_pairs(words_in_message):
-    for i in range(len(words_in_message)- 1):
-            yield (words_in_message[i], words_in_message[i + 1])
-
-
-def add_to_dick(words_in_message, chat_id):
-    pair_of_words = make_pairs(words_in_message)
-    for word_0, word_1 in pair_of_words:
-        search = "SELECT * FROM dickdump WHERE chat_id=? AND word_0=?"
-        cursor.execute(search, [(chat_id), (word_0)])
-        search_result = (cursor.fetchone())
-        if search_result == None:
-            cursor.execute("INSERT INTO dickdump VALUES (?,?,?)", (chat_id, word_0, repr([word_1])))
-        else:
-            search_result_word_0 = search_result[1]
-            search_result_word_1 = eval(search_result[2])
-            if word_1 in search_result_word_1:
-                pass
-            else:
-                search_result_word_1.append(word_1)
-                cursor.execute("UPDATE dickdump SET word_1=? WHERE word_0=?", (repr(search_result_word_1), search_result_word_0))
+    cursor.execute("INSERT INTO dickdump VALUES (?,?,?)", (chat_id, "Добрый", repr({"День": 1})))
     conn.commit()
-
-
-def next_word(first_word, chat_id):
-    search = "SELECT word_1 FROM dickdump WHERE chat_id=? AND word_0=?"
-    cursor.execute(search, [(chat_id), (first_word)])
-    search_result = (cursor.fetchone())
-    if search_result == None:
-        return None
-    else:
-        search_result_word_1 = random.choice(eval(search_result[0]))
-        return(search_result_word_1)
-
 
 def message_handler(update, context):
     chat_id = update.message.chat.id
-    message = re.sub(r"http\S+", " ", update.message.text)
-    message = re.sub(r"\S*@\S*\s?", " ", message)
-    message = re.sub(r"\n", " ", message)
-    words_in_message = message.split(' ')
-    add_to_dick(words_in_message, chat_id)
-
-
+    message = update.message.text
+    print("\nИзначальное сообщение -- ", message, "\n")
+    add_words_in_message_to_dictionary(message, chat_id)
     if random.random() < markov_chance/100 or (update.message.reply_to_message != "None" and update.message.reply_to_message.from_user.username == "puklengtime_bot"):
-        random_index = random.randrange(0, (len(words_in_message)))
-        first_word = words_in_message[random_index]
-        while first_word.isalpha() == False:
-            words_in_message.pop(random_index)
-            random_index = random.randrange(0, (len(words_in_message)))
-            first_word = words_in_message[random_index]
-        chain = [first_word]
-        next_word_var = next_word(first_word, chat_id)
-        n_words = random.randint(2, 15)
-        for i in range(n_words):
-            next_word_var = next_word(first_word, chat_id)
-            if next_word_var == None:
-                pass
-            else:
-                chain.append(next_word_var)
-                first_word = next_word_var
-        exit_message = ' '.join(chain)
-        exit_message = exit_message.replace(" ,", ", ").replace(" .",". ").replace(" -","-").replace(" ?","? ").replace(" !","! ").replace(" «","«").replace(" »","»").replace(" ;","; ").replace("  "," ")
-        context.bot.send_message(chat_id = update.message.chat_id, text = exit_message)
+        generate_shitpost('message', chat_id)
+    else:
+        pass
+    
 
 def main():
     updater = Updater(token, use_context=True)
--- generator.py
+++ generator.py
@@ -1,33 +1,48 @@
-import sqlite3, random
+import sqlite3, random, re
 
-conn = sqlite3.connect("dickdump.db")
+conn = sqlite3.connect("dickdump.db", check_same_thread=False)
 cursor = conn.cursor()
-cursor.execute("""CREATE TABLE  IF NOT EXISTS   dickdump(chat_id integer, word_0 text, word_1 text)""")
 
-
-def make_pairs(words_in_message):
-    for i in range(len(words_in_message)- 1):
+def add_words_in_message_to_dictionary(message, chat_id):
+    message = re.sub(r"http\S+", " ", message)
+    message = re.sub(r"\S*@\S*\s?", " ", message)
+    message = re.sub(r"\n", " ", message)
+    words_in_message = message.split(" ")
+    print(words_in_message)
+    def make_pairs(words_in_message):
+        for i in range(len(words_in_message)- 1):
             yield (words_in_message[i], words_in_message[i + 1])
 
-
-def add_word_to_dictionary(words_in_message, chat_id):
     pair_of_words = make_pairs(words_in_message)
+    print(pair_of_words)
+
     for word_0, word_1 in pair_of_words:
         search = "SELECT * FROM dickdump WHERE chat_id=? AND word_0=?"
         cursor.execute(search, [(chat_id), (word_0)])
         search_result = (cursor.fetchone())
         if search_result == None:
-            cursor.execute("INSERT INTO dickdump VALUES (?,?,?)", (chat_id, word_0, repr([word_1])))
+            cursor.execute("INSERT INTO dickdump VALUES (?,?,?)", (chat_id, word_0, repr({word_1: 1})))
         else:
             search_result_word_0 = search_result[1]
+            search_result_word_1 = dict
             search_result_word_1 = eval(search_result[2])
-            if word_1 in search_result_word_1:
-                pass
+            if word_1 in search_result_word_1.keys():
+                search_result_word_1.update({word_1: (search_result_word_1.get(word_1)+1)})
+                print(search_result_word_1)
+                cursor.execute("UPDATE dickdump SET word_1=? WHERE word_0=?", (repr(search_result_word_1), search_result_word_0))
             else:
-                search_result_word_1.append(word_1)
+                search_result_word_1.update({word_1:1})
                 cursor.execute("UPDATE dickdump SET word_1=? WHERE word_0=?", (repr(search_result_word_1), search_result_word_0))
     conn.commit()
 
+def generate_shitpost(message, chat_id):
+    print("hello")
+
+
+
+
+
+'''
 def next_word(first_word, chat_id):
     search = "SELECT word_1 FROM dickdump WHERE chat_id=? AND word_0=?"
     cursor.execute(search, [(chat_id), (first_word)])
@@ -38,5 +53,25 @@ def next_word(first_word, chat_id):
         search_result_word_1 = random.choice(eval(search_result[0]))
         return(search_result_word_1)
 
-def markov_generator(message):
-    print(message)
+
+if random.random() < markov_chance/100 or (update.message.reply_to_message != "None" and update.message.reply_to_message.from_user.username == "puklengtime_bot"):
+        random_index = random.randrange(0, (len(words_in_message)))
+        first_word = words_in_message[random_index]
+        while first_word.isalpha() == False:
+            words_in_message.pop(random_index)
+            random_index = random.randrange(0, (len(words_in_message)))
+            first_word = words_in_message[random_index]
+        chain = [first_word]
+        next_word_var = next_word(first_word, chat_id)
+        n_words = random.randint(2, 15)
+        for i in range(n_words):
+            next_word_var = next_word(first_word, chat_id)
+            if next_word_var == None:
+                pass
+            else:
+                chain.append(next_word_var)
+                first_word = next_word_var
+        exit_message = ' '.join(chain)
+        exit_message = exit_message.replace(" ,", ", ").replace(" .",". ").replace(" -","-").replace(" ?","? ").replace(" !","! ").replace(" «","«").replace(" »","»").replace(" ;","; ").replace("  "," ")
+        context.bot.send_message(chat_id = update.message.chat_id, text = exit_message)
+'''
